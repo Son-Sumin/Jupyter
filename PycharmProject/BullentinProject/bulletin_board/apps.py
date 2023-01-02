83723985efdd9bdbe30653c0e9b2d294908bb767
Son-Sumin <goldkitty007@gmail.com>
2023-01-03 08:47:25 +0900

    구조수정

--- PycharmProject/BullentinProject/bulletin_board/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-# startapp 할 때마다 자동으로 추가가 됨
-
-class BulletinBoardConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'bulletin_board'
